057f34b1d2811096a7c1aac480efc082a43dff5d
j4m1n-t <j4m1n_t@pm.me>
2024-07-09 16:57:31 -0500

    email to mailto:

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 export const LINKS = {
   github: "https://github.com/j4m1n-t",
   discord: "https://discordapp.com/users/163300027618295808",
+  email: "mailto:j.thompson@j4m1n.me",
 };
 
 export const loaderAnimation = [
