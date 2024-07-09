0ac9bd84c9c319fea88d62968842ad2d8944ebf9
j4m1n-t <j4m1n_t@pm.me>
2024-07-09 16:26:21 -0500

    updated images and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,13 +12,13 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
+  site: "https://j4m1n.me/",
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
+        "https://j4m1n.me/sitemap-index.xml",
+        "https://j4m1n.me/sitemap-0.xml",
       ],
     }),
     solidJs(),
