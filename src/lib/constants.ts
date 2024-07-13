d64cf63632e77e0cbcaeeabb7b1888429a41269f
Jamin Thompson <76403023+j4m1n-t@users.noreply.github.com>
2024-07-12 20:56:05 -0500

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
   github: "https://github.com/j4m1n-t",
-  discord: "https://discordapp.com/users/163300027618295808",
+  discord: "https://discordapp.com/users/jamin_thompson_97890",
   email: "mailto:j.thompson@j4m1n.me",
 };
 
